2d369a79129730dd52ba5c3d2417d6beccf6c0c2
lakkam27 <lakkamsaiakhil@gmail.com>
2024-06-01 20:14:22 +0530

    spring boot application 2

--- order-service/src/main/java/com/Lakkam/order_service/Repository/OrderRepsoitory.java
+++
@@ -1,7 +0,0 @@
-package com.Lakkam.order_service.Repository;
-
-import com.Lakkam.order_service.Model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepsoitory extends JpaRepository<Order,Long> {
-}
--- product-service/src/main/java/com/Lakkam/product_service/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Lakkam.product_service.Repository;
-
-import com.Lakkam.product_service.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,String> {
-}
